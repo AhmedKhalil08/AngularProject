ffd7cba9c44054d6a3f143b94b4b12b05f85b642
arwa <arwaalaa2001@gmail.com>
2026-05-12 14:09:11 +0300

    Errors fixed.

--- ECommerce.Infrastructure/Migrations/20260512110237_InitialCreate.Designer.cs
+++ ECommerce.Infrastructure/Migrations/20260512110237_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerce.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260512053355_Init")]
-    partial class Init
+    [Migration("20260512110237_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
