f0215fe447a265ca4a8b0f1339a4b0af9a148de9
Ahmed-Khail <ahmed.mohamed.hamdy08@gmail.com>
2026-05-15 15:11:28 +0300

    Revert "Merge branch 'arwa' into Dev"
    This reverts commit 8335b4aaebd7ecea629790cc34505bc799f7fe4a, reversing
    changes made to 73ec330b745a714d90bf1ce4f6bab6f2aab4d132.

--- ECommerce.Client/src/app/app.ts
+++ ECommerce.Client/src/app/app.ts
@@ -1,25 +1,19 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/services/auth.service';
-import {PromoCode} from './features/admin/components/promo-code/promo-code'
-import {Category} from './features/admin/components/category/category'
-import {BnrCarousel} from './features/home/components/bnr-carousel/bnr-carousel'
-import {Footer} from './shared/components/footer/footer'
-
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PromoCode,Category,BnrCarousel,Footer],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   protected readonly title = signal('ECommerce.Client');
 constructor(private authService:AuthService){}
-   ngOnInit() {}
-  //   this.authService.loadCurrentUser().subscribe({
-  //     error : ()=> {}
-  //   });
-  // };
+  ngOnInit() {
+    this.authService.loadCurrentUser().subscribe({
+      error : ()=> {}
+    });
+  };
 }
--- ECommerce.Client/src/app/shared/pipes/date-clean-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateCleanPipe } from './date-clean-pipe';
-
-describe('DateCleanPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateCleanPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
